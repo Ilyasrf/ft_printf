ba5b61211eeb3c854e49da50c8f3dbff16eef014
Ilyas Rfei <ilyasilyas17062003@gmail.com>
2024-11-25 22:46:48 +0100

    ft_printf

--- bonus/ft_printf_bonus.c
+++ bonus/ft_printf_bonus.c
@@ -6,7 +6,7 @@
 /*   By: irfei <irfei@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 15:46:41 by irfei             #+#    #+#             */
-/*   Updated: 2024/11/24 16:30:36 by irfei            ###   ########.fr       */
+/*   Updated: 2024/11/25 21:45:17 by irfei            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,43 @@ void f(const char format_char, va_list args, int *char_count)
 		*char_count += ft_putchar_bonus(va_arg(args, int));
 	else if (format_char == 's')
 		*char_count += ft_putstr_bonus(va_arg(args, char*));
-	else if (format_char == 'd')
+	else if (format_char == 'd' || format_char == 'i')
+		*char_count += ft_putnbr_bonus(va_arg(args, int));
+	else if (format_char == 'x')
+		*char_count += ft_putnbr_hex_bonus(va_arg(args, unsigned int));
+	else if (format_char == 'X')
+		*char_count += ft_putnbr_hex_upper_bonus(va_arg(args, unsigned int));
+	else if (format_char == 'p')
+	{
+		*char_count += ft_putstr_bonus("0x");
+		*char_count += ft_putptr_bonus(va_arg(args, unsigned long));
+	}
+	else if (format_char == 'u')
+		*char_count += ft_putnbr_unsigned_bonus(va_arg(args, unsigned int));
+	else if (format_char == '%')
+		*char_count += ft_putpercent_bonus();
+}
+int ft_printf(const char *format, ...)
+{
+	va_list args;
+	int char_count;
+	int i;
+	char_count = 0;
+	i = 0;
+	va_start(args, format);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			
+			f(format[i], args, &char_count);
+		}
+		else
+			char_count += ft_putchar_bonus(format[i]);
+		i++;
 		
+	}
+	va_end (args);
+	return (char_count);
 }
--- bonus/ft_printf_bonus.h
+++ bonus/ft_printf_bonus.h
@@ -6,7 +6,7 @@
 /*   By: irfei <irfei@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 13:37:27 by irfei             #+#    #+#             */
-/*   Updated: 2024/11/24 15:44:33 by irfei            ###   ########.fr       */
+/*   Updated: 2024/11/25 22:36:34 by irfei            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int	ft_printf_bonus(const char *format, ...);
+int ft_printf(const char *format, ...);
 int	ft_putchar_bonus(char c);
-int	ft_putnbr_bonus(int nb);
+int ft_putnbr_bonus(int nb, char flag);
 int	ft_putstr_bonus(const char *str);
 int	ft_putnbr_unsigned_bonus(unsigned int nb);
-int	putnbr_hex_bonus(unsigned int nb);
+int	ft_putnbr_hex_bonus(unsigned int nb);
 int	ft_putptr_bonus(unsigned long nb);
 int	ft_putpercent_bonus(void);
 int	ft_putnbr_hex_upper_bonus(unsigned int nb);
